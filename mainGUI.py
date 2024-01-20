2dfb74668653d7d51532571af48c6ba388610248
khanhkhanh1210 <khanh.nguyentom@hcmut.edu.vn>
2024-01-20 15:34:09 +0700

    delete some code

--- mainGUI.py
+++ mainGUI.py
@@ -21,34 +21,11 @@ class CameraGUI(tk.Frame):
         self.capture = tk.Button(self, text="Capture", command=self.capture)
         self.capture.pack(side="bottom")
 
-
-#HISTOGRAM gui
-class HistogramGUI(tk.Frame):
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
-        self.pack()
-        self.create_widgets()
-    def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Histogram"
-        self.hi_there["command"] = self.say_hi
-        self.hi_there.pack(side="top")
-        self.quit = tk.Button(self, text="QUIT", fg="red", command=self.master.destroy)
-        self.quit.pack(side="bottom")
-    def say_hi(self):
-        print("hi there, everyone!")
 #MAIN
 def showCamera():
     camera = CameraGUI(master=root)
     camera.mainloop()
 
-def showApp():
-    app = HistogramGUI(master=root)
-    app.mainloop()
-
 if __name__ == "__main__":
     showCamera()
-    showApp()
-
     print("Application executed.")
