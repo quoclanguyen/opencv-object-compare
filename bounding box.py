de848840087aae940c64937dad09ed9d9c23c093
khanhkhanh1210 <khanh.nguyentom@hcmut.edu.vn>
2024-01-26 22:25:38 +0700

    Refactor bounding box drawing and grabcut algorithm

--- bounding box.py
+++ bounding box.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def grabcut_algorithm(original_image, bounding_box):
-    
+
     segment = np.zeros(original_image.shape[:2],np.uint8)
     
     x,y,width,height = bounding_box
@@ -16,39 +16,40 @@ def grabcut_algorithm(original_image, bounding_box):
     cv2.GC_INIT_WITH_RECT)
 
     new_mask = np.where((segment==2)|(segment==0),0,1).astype('uint8')
-
     original_image = original_image*new_mask[:,:,np.newaxis]
-
     cv2.imshow('Result', original_image)
 
 def draw_bounding_box(click, x, y, flag_param, parameters):
-    global x_pt, y_pt, drawing, top_left_point, bottom_right_point, original_image  
-    if click == cv2.EVENT_LBUTTONDOWN:
+    global x_pt, y_pt, drawing, top_left_point, bottom_right_point, original_image, cached_image, image
+    if click == cv2.EVENT_LBUTTONDOWN: # Left button click
         drawing = True
-        x_pt, y_pt = x, y   
+        x_pt, y_pt = x, y 
 
     elif click == cv2.EVENT_MOUSEMOVE:
         if drawing:
             top_left_point, bottom_right_point = (x_pt,y_pt), (x,y)
-            image[y_pt:y, x_pt:x] = 255 - original_image[y_pt:y, x_pt:x]
-            cv2.rectangle(image, top_left_point, bottom_right_point, (0,255,0), 2)
-    
+            image[y_pt:y, x_pt:x] = 255 - original_image[y_pt:y, x_pt:x] 
+            cv2.rectangle(image, top_left_point, bottom_right_point, (0,255,0), 2) # Draw rectangle on the image
+
     elif click == cv2.EVENT_LBUTTONUP:
         drawing = False
         top_left_point, bottom_right_point = (x_pt,y_pt), (x,y)
         image[y_pt:y, x_pt:x] = 255 - image[y_pt:y, x_pt:x]
         cv2.rectangle(image, top_left_point, bottom_right_point, (0,255,0), 2)
-        bounding_box = (x_pt, y_pt, x-x_pt, y-y_pt)
-        
+        bounding_box = (x_pt, y_pt, x-x_pt, y-y_pt) 
         grabcut_algorithm(original_image, bounding_box)
-
+    #delete the bounding box
+        image = cached_image.copy() # Make a copy of original image to draw bounding box
+    
 if __name__=='__main__':
     drawing = False
     top_left_point, bottom_right_point = (-1,-1), (-1,-1)
 
     original_image = cv2.imread("robert.png")
-    original_image = cv2.resize(original_image ,(500,500))
-    image = original_image.copy()
+    
+    original_image = cv2.resize(original_image ,(500,500)) # Resize image
+    cached_image = original_image.copy()
+    image = original_image.copy() # Make a copy of original image to draw bounding box
     cv2.namedWindow('Frame')
     cv2.setMouseCallback('Frame', draw_bounding_box)
 
